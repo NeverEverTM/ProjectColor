af13fd164c7e23ca2e2ad5fb84215a07634c30af
Summer <119933364+NeverEverTM@users.noreply.github.com>
2024-01-20 12:50:46 -0600

    Delete _site directory
    .gitignore

--- _site/html/blog-fill.js
+++
@@ -1,9 +0,0 @@
-var blogs = document.getElementById("blog");
-var i;
-    
-for (i = 0; i < blogs.length; i++) 
-{
-    blogs[i].getElementById("blog-name").content = "Name"
-    blogs[i].getElementById("blog-content").content = "Content"
-    blogs[i].getElementById("blog-date").content = "Date"
-}
