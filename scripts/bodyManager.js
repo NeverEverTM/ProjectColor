605cc35a69250c0e59faddce54c4b108e50e09a1
Summer <danytravi2011@hotmail.com>
2024-02-10 01:39:53 -0600

    updated blog manager so links actually takes you to the corresponding tab/blog; 404 error message for said link ref

--- scripts/bodyManager.js
+++ scripts/bodyManager.js
@@ -3,7 +3,10 @@ class BodyManager
     static currentBodyId = "";
     static categoryBody = document.getElementById("category-body");
 
-    static ChangeCategory(_category, _isBlog = false)
+    // TODO: changing blog loading, so is done in a separate html page as the Home one
+    // This should solve the hash not working as well as setting manually some blog content on the head
+
+    static ChangeCategory(_category, _type = "body")
     {
         if (BodyManager.currentBodyId == _category)
             return;
@@ -12,10 +15,35 @@ class BodyManager
         while (BodyManager.categoryBody.hasChildNodes())
             BodyManager.categoryBody.removeChild(BodyManager.categoryBody.firstChild);
 
-        if (_isBlog)
-            $("#category-body").load("./html/blogs/" + BodyManager.currentBodyId + "-content.html");
-        else
-            $("#category-body").load("./html/body-" + BodyManager.currentBodyId + ".html");
+        try
+        {
+            switch(_type)
+            {
+                case "body":
+                    $("#category-body").load("./html/body-" + BodyManager.currentBodyId + ".html");
+                    break;
+                case "blog":
+                    $("#category-body").load("./html/blogs/" + BodyManager.currentBodyId + "-content.html");
+                    break;
+            }
+            var _url = new URL(window.location.href);
+            var _hash;
+            _url.searchParams.set("id", _category)
+            _url.searchParams.set("type", _type)
+
+            if (history.pushState)
+                window.history.pushState({},'', _url);
+        }
+        catch(e)
+        {
+            $("#category-body").load("./html/blogs/test-content.html");
+            console.log(e);
+        }
     }
 }
-BodyManager.ChangeCategory("home");
+
+var _search = new URLSearchParams(window.location.search);
+var _id = _search.has("id") ? _search.get("id") : "home";
+var _type = _search.has("type") ? _search.get("type") : "body";
+
+BodyManager.ChangeCategory(_id, _type);
